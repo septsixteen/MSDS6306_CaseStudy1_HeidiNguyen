582ff693f4b8a0501d5d0183a6cea0c50666e250
Heidi Nguyen <heidinguyen@Heidis-MacBook-Pro.local>
2017-03-22 05:56:23 -0700

    commit 4: final

--- Analysis/casestudy1_analyze_data.R
+++ Analysis/casestudy1_analyze_data.R
@@ -1,8 +1,8 @@
-##################################
+###################################
 #MSDS6306_CaseStudy1_HeidiNguyen
 #Date: 3/23/2017 
-#PART 3:Merging and Analyzing data                                                 
-##################################
+#PART 3: Merging and Analyzing Data                                                 
+###################################
 
 #3A. MERGING THE CLEAN GDP AND EDUCATIONAL DATA BASED ON COUNTRY SHORTCODE
 #set the directory
@@ -10,36 +10,18 @@ setwd("/Volumes/NO NAME/Data Science/2016-0831 MSDS 6306 Doing Data Science/Unit
 
 #read in the clean GDP data. 
 GDPclean <- read.csv("GDPclean.csv", encoding="latin1", stringsAsFactors=FALSE, header=TRUE, na.strings=c("NA", "NULL"))
-#examine the data 
-dim(GDPclean)
-names(GDPclean)
-str(GDPclean)
-head(GDPclean, 20)
-tail(GDPclean, 20)
 #remove the numbering column 1 and the  groupnote column 6
 GDPclean <- GDPclean[,c(2:5)]
-str(GDPclean)
 
 #read in the clean Educational data
 educlean <- read.csv("educlean.csv", encoding="latin1", stringsAsFactors=FALSE, header=TRUE, na.strings=c("NA", "NULL"))
-#examine the data 
-dim(educlean)
-names(educlean)
-str(educlean)
-head(educlean, 20)
-tail(educlean, 20)
 #remove the numbering column 1
-educlean <- educlean[,c(2:3)] 
-str(educlean)
+educlean <- educlean[,c(2:3)]
 
 # Merge the data on "CountryCode" 
 clean <- merge(GDPclean, educlean, by="CountryCode", all=TRUE)
 #check for duplicates of CountryCode
 table(clean$CountryCode)[table(clean$CountryCode) != 1]
-#check the merged data 
-str(clean)
-head(clean)
-tail(clean)
 
 # write the clean education data set and save in the same /Analysis folder
 setwd("/Volumes/NO NAME/Data Science/2016-0831 MSDS 6306 Doing Data Science/Unit 8/CaseStudy1/MSDS6306_CaseStudy1_HeidiNguyen/Analysis")
@@ -75,13 +57,14 @@ detach(matchID)
 
 #Question 3: Find the average GDP rankings for the "High income: OECD" and "High income: nonOECD" groups
 #using mean() to find the average GDP rankings for the "High income: OECD"
-mean(matchIDsorted[matchIDsorted$IncomeGroup == "High income: OECD","rank"]) #32.96667
+mean(matchIDsorted[matchIDsorted$IncomeGroup == "High income: OECD","rank"]) # 32.96667
 #using mean to find the average GDP rankings for the "High income: nonOECD" 
 mean(matchIDsorted[matchIDsorted$IncomeGroup == "High income: nonOECD","rank"]) #91.91304
 
-#Question 4: Show the distribution of GDP value for all the countries and color plots by income group using ggplot2.
-#Load the ggplot2 package
+#Question 4: Show the distribution of GDP value for all the countries and color plots by income group usin ggplot2.
+#Load the ggplot2 and imager packages
 library(ggplot2)
+library(imager)
 #use geom_boxplot with option fill=IncomeGroup to show the distribution of GDP value for all the countries and color plots by income group
 #add theme_update at the beginning to center the title before any plot is created
 #add ggsave to save the plot and adjust the window dpi to show ylabel
@@ -95,31 +78,38 @@ boxplotGPD + title.boxplot
 #save the box plox
 saveboxplotGDP <- ggsave(file = "boxplot.png", dpi = 500, width = 12, height = 8, units = "in")
 
-#Due to the skewness of the data and the order of magnitude is of factor of 10, I will transform y to log10(y)
+#file <- system.file('boxplot.png',package='imager')
+#im <- load.image(file)
+#system.file gives the full path for a file that ships with a R package
+#if you already have the full path to the file you want to load just run:
+boxplot <- load.image("boxplot.png")
+
+
+#Due to the skewness of the data and the order of magnitude of the data is of factor of 10, I will transform y to log10(y)
 #load library scale to transform and format y-axis scale
 library(scales)
 transform.y <- scale_y_continuous(trans='log10', name = "GDP in millions USD - log10 scaling")
 title.boxplot.log <- ggtitle("GDP by Income Group - log10 scaling")
+stat_sum <- stat_summary(fun.y=mean, colour="darkred", geom="point",shape=18, size=3,show.legend = FALSE)
+
 #boxplot of log10(GDP) vs income group
-boxplotGPD + transform.y + title.boxplot.log 
+boxplotGPD + transform.y + title.boxplot.log + stat_sum
+
 #save the log10 box plot
 saveboxplot <- ggsave(file = "log10boxplot.png", dpi = 500, width = 12, height = 8, units = "in")
+boxplotlog <- load.image("log10boxplot.png")
 
 #Question 5: Summary statistics of GDP by income groups.
 #use tapply() to show the summary of GDP (GDP2012millionsUSD) by income groups (IncomeGroup)
 tapply(matchIDsorted$GDP2012millionsUSD, matchIDsorted$IncomeGroup, summary)
 #to get scientific notation, use a custom function instead of summary such as
-tapply(matchIDsorted$GDP2012millionsUSD, matchIDsorted$IncomeGroup, function(x) format(summary(x), scientific = TRUE))
+GPDstats <- tapply(matchIDsorted$GDP2012millionsUSD, matchIDsorted$IncomeGroup, function(x) format(summary(x), scientific = TRUE))
+
 
 #Question 6: Cut the GDP ranking into 5 separate quantile groups. Make a table versus Income.Group. Find the number of countries are Lower middle income but among the 38 nations with highest GDP.
 #6a: use cut() and quantile() to separate the rankings into 5 groups
 matchIDsorted$quantile  <- with(matchIDsorted, cut(matchIDsorted$rank, 
                                                    breaks=quantile(matchIDsorted$rank, probs=seq(0,1, by=0.20), na.rm=TRUE), labels=c("Q1","Q2","Q3","Q4", "Q5"),include.lowest=TRUE))
-#review the data
-str(matchIDsorted)
-summary(matchIDsorted)
-head(matchIDsorted)
-tail(matchIDsorted)
 
 #6b: Table of quantile versus income group
 tablequantileincomegroup <- with(matchIDsorted, table(matchIDsorted$quantile, matchIDsorted$IncomeGroup))
@@ -129,7 +119,7 @@ tablequantileincomegroup
 tablehighGDPlowmiddleincome <- with(highGDPlowmiddleincome<-matchIDsorted[(matchIDsorted$quantile=="Q1" & matchIDsorted$IncomeGroup=="Lower middle income"),], table(highGDPlowmiddleincome$quantile,highGDPlowmiddleincome$IncomeGroup))
 #table of countries that are Lower middle income but among the 38 nations with highest GDP.
 tablehighGDPlowmiddleincome
-#number of countries: 
+#number of countries that are classified as Lower middle income but among the 38 nations with the highest GDP
 tablehighGDPlowmiddleincome[tablehighGDPlowmiddleincome>0]
 
        
--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# MSDS6306_CaseStudy1_HeidiNguyen
+# MSDS6306_CaseStudy1_HeidiNguyen:
+
+I. Introduction: 
+
+This repository is a reproducible project using R, Rmarkdown, GitHub, and Git for MSDS 6306 Case Study I.  
+This project explores and analyzes the 2012 global GPD rank, GDP value, and income group. 
+Two datasets, the 2012 Gross Domestic Product for the 190 ranked countries and the global education data set that
+contained income levels by country, were downloaded from The Data World Bank website.
+The two datasets were then cleaned and merged based on the short country codes. 
+An analysis of the merged data was completed using GDP, and Income Group. The analysis answered the following
+questions: 
+
+    1. Merge the data based on the country shortcode and identify how many of the IDs matched. 
+    2. Sort the data frame in ascending order by GDP and identify the 13th country in the resulting data frame.
+    3. Show the average GDP rankings for the "High income: OECD" and "High income: nonOECD" groups.
+    4. Show the distribution of GDP value for all the countries and color plots by income group using ggplot2.
+    5. Summary statistics of GDP by income groups.
+    6. Cut the GDP ranking into 5 separate quantile groups. Make a table versus Income Group and find out how many countries are Lower middle income but among the 38 nations with highest GDP. 
+
+II. File structure 
+
+--  Analysis
+    --  boxplot.png             
+    --  casestudy1_analyze_data.R
+    --  log10boxplot.png
+    --  mergedata.csv 
+--  Data
+    --  casestudy1_clean_data.R    
+    --  casestudy1_download_data.R
+    --  educlean.csv              
+    --  eduraw.csv               
+    --  GDPclean.csv            
+    --  GDPraw.csv  
+--  Paper
+    --  CaseStudy1_GDPstudy_HeidiNguyen.html
+    --  CaseStudy1_GDPstudy_HeidiNguyen.md
+    --  CaseStudy1_GDPstudy_HeidiNguyen.Rmd
+    --  CaseStudy1_GDPstudy_HeidiNguyen_files
+        -- figure-html
+            -- unnamed-chunk-7-1.png
+            -- unnamed-chunk-8-1.png
+--  README.md 
+
+
+
+
+
